e35f984b6c0f168727a17416ce6607a6ef56a63e
sgolivernet <sgoliver.net@gmail.com>
2013-08-10 12:35:24 +0200

    Navigation Drawer

---
+++ android-navigationdrawer/src/net/sgoliver/android/navdrawer/Fragment2.java
@@ -0,0 +1,17 @@
+package net.sgoliver.android.navdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.fragment_2, container, false);
+	}
+}
